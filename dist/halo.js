d5969abfa23aaf18658d33da946ef18ca86fef35
Todd Wolfson <todd@twolfson.com>
2013-01-15 04:34:55 -0800

    Fixed unnecesary global lookup for socket.io

--- dist/halo.js
+++ dist/halo.js
@@ -16357,7 +16357,7 @@ define("../../models/CrudModel.js",['Sauron'], function (Sauron) {
   return CrudModel;
 });
 /*global io:true*/
-define("../../models/SocketModel.js",['Sauron', 'mvc!m/CrudModel', 'socket.io'], function (Sauron, CrudModel) {
+define("../../models/SocketModel.js",['Sauron', 'mvc!m/CrudModel', 'socket.io'], function (Sauron, CrudModel, io) {
   // Create a socket which proxies all requests
   var href = window.location.href,
       isSecure = href.slice(0, 5) === 'https',
